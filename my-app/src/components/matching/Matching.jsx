0e64c76f8b39bd268f6b5945192aead6925f884a
hgu7 <91969855+hgu7@users.noreply.github.com>
2022-10-16 00:13:55 -0400

    add matching form

---
+++ my-app/src/components/matching/Matching.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MatchingForm from './MatchingForm';
+
+const Matching = () => {
+    return (
+        <div>
+            <MatchingForm />
+        </div>
+    );
+}
+
+export default Matching;

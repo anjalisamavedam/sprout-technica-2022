9b7d42eeeee8085886396b793fad1538ad7b3726
hgu7 <91969855+hgu7@users.noreply.github.com>
2022-10-16 02:01:53 -0400

    matching

--- my-app/src/components/navbar/Navbar.jsx
+++ my-app/src/components/navbar/Navbar.jsx
@@ -14,6 +14,7 @@ function Navbar(){
                     <li><a href="../mentor">Mentor</a></li>
                     <li><a href="../mentee">Mentee</a></li>
                     <li><a href="../internship">Internship</a></li>
+                    <li><a href="../matching">Matching</a></li>
                </ul>
 
            </div>

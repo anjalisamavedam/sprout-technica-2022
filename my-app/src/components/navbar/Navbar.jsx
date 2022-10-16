36dbe04d2769744edef79bc62274b892ec9cdf88
Beatrice Chung <beatr@wireless-10-105-221-225.umd.edu>
2022-10-16 07:14:58 -0400

    logos, landing page

--- my-app/src/components/navbar/Navbar.jsx
+++ my-app/src/components/navbar/Navbar.jsx
@@ -1,14 +1,18 @@
 import "./Navbar.css";
+// import image from '../img/sproutLogo.PNG';
 
 function Navbar(){
     return(
        <div>
            <nav>
                 <ul id="navbar">
-                    <li>sprout</li>
+                    {/* <li>sprout<img src={image} height={100} width={100}></img>
+</li> */}
+                <li>sprout</li>
                 </ul>
 
            <div>
+
                <ul id="navbar">
                     <li><a href="index.html">Home</a></li>
                     <li><a href="../mentor">Mentor</a></li>

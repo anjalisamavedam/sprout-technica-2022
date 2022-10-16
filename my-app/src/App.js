b26140988b6064dc2a4c4dd2fe8670266d769449
Jennifer Li <jenniferli@wireless-10-105-48-219.umd.edu>
2022-10-16 02:18:25 -0400

    resolved internship/matching conflict

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,12 +5,7 @@ import Navbar from './components/navbar/Navbar';
 
 import Mentee from './components/mentee/Mentee';
 import Mentor from './components/mentor/Mentor';
-<<<<<<< HEAD
-=======
-import Internship from './components/internship/Internship';
 import Matching from './components/matching/Matching';
->>>>>>> 9b7d42eeeee8085886396b793fad1538ad7b3726
-
 
 function App() {
   return (
@@ -23,11 +18,7 @@ function App() {
           <Route path='/' element={<App/>}></Route>
           <Route path='/mentee' element={<Mentee/>}></Route>
           <Route path='/mentor' element={<Mentor/>}></Route>
-<<<<<<< HEAD
-=======
-          <Route path='/internship' element={<Internship/>}></Route>
           <Route path='/matching' element={<Matching/>}></Route>
->>>>>>> 9b7d42eeeee8085886396b793fad1538ad7b3726
         </Routes>
       </Router>
 
--- my-app/src/components/navbar/Navbar.jsx
+++ my-app/src/components/navbar/Navbar.jsx
@@ -13,11 +13,7 @@ function Navbar(){
                     <li><a href="index.html">Home</a></li>
                     <li><a href="../mentor">Mentor</a></li>
                     <li><a href="../mentee">Mentee</a></li>
-<<<<<<< HEAD
-=======
-                    <li><a href="../internship">Internship</a></li>
                     <li><a href="../matching">Matching</a></li>
->>>>>>> 9b7d42eeeee8085886396b793fad1538ad7b3726
                </ul>
 
            </div>

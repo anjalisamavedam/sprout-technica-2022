736ac164d6f263c13c8f19c6f2d28eac868df13d
Jennifer Li <jenniferli@wireless-10-105-48-219.umd.edu>
2022-10-16 00:36:07 -0400

    update

--- my-app/src/components/internship/JobCard.jsx
+++ my-app/src/components/internship/JobCard.jsx
@@ -6,13 +6,18 @@ const JobCard = (props) => {
     <div className='jobcard'>
       <p><strong>Position:</strong> {props.position}, <strong>Company:</strong> {props.company}</p>
       <p><strong>Position description:</strong> {props.description}</p>
-      <PieChart className='pie-chart'
-        data={[
-          { title: 'One', value: 10, color: '#E38627' },
-          { title: 'Two', value: 15, color: '#C13C37' },
-          { title: 'Three', value: 20, color: '#6A2135' },
-        ]}
-      />
+      <div className='demographics'>
+
+        <PieChart className='piechart'
+          data={[
+            { title: 'One', value: 10, color: '#E38627' },
+            { title: 'Two', value: 15, color: '#C13C37' },
+            { title: 'Three', value: 20, color: '#6A2135' },
+          ]}
+        />
+        <p>48% of employees at {props.company} are women.</p>
+
+      </div>
 
 
 
